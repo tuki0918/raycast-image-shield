8d3261b68e5f2de597581f0202afc398e42447a5
Y.Yamamoto <tuki0918@gmail.com>
2025-06-14 06:48:21 +0900

    feat: create Command component for image decryption workflow

---
+++ src/decrypt-images.tsx
@@ -0,0 +1,41 @@
+import { Detail, showToast, Toast } from "@raycast/api";
+import { usePromise } from "@raycast/utils";
+import PasswordForm from "./components/PasswordForm";
+import GridLoadingView from "./components/GridLoadingView";
+import RestoredImagesGrid from "./components/RestoredImagesGrid";
+import { useDecryptImages } from "./hooks/useDecryptImages";
+
+export default function Command() {
+  const { isLoading, error, data, selectedManifest, selectedImagePaths, secretKey, initialize, handleDecrypt } =
+    useDecryptImages();
+
+  // Initialize
+  usePromise(async () => await initialize(), []);
+
+  // Error toast
+  if (error) {
+    showToast({
+      style: Toast.Style.Failure,
+      title: "decrypting failed",
+      message: error,
+    });
+  }
+
+  // Loading
+  if (isLoading) {
+    return <GridLoadingView title="decrypting..." />;
+  }
+
+  // Password form
+  if (selectedManifest?.secure && !secretKey) {
+    return <PasswordForm onSubmit={(secretKey) => handleDecrypt(selectedManifest, selectedImagePaths, secretKey)} />;
+  }
+
+  // Restored images grid
+  if (data) {
+    return <RestoredImagesGrid manifest={data.manifest} imageBuffers={data.imageBuffers} />;
+  }
+
+  // Default view
+  return <Detail markdown="please select images" />;
+}
