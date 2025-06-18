84b1fc4745eaea616bb023ee7fba455c39dd5639
Y.Yamamoto <tuki0918@gmail.com>
2025-06-19 03:31:27 +0900

    fix: change default value for restore original file name in settings to true

--- src/constraints.ts
+++ src/constraints.ts
@@ -6,4 +6,4 @@ export const SETTINGS_KEY = "settings";
 export const SETTINGS_DEFAULT_BLOCK_SIZE = "4";
 export const SETTINGS_DEFAULT_PREFIX = "img";
 export const SETTINGS_DEFAULT_ENCRYPTED = true;
-export const SETTINGS_DEFAULT_RESTORE_FILE_NAME = false;
+export const SETTINGS_DEFAULT_RESTORE_FILE_NAME = true;
